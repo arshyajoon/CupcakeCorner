a815da7069a3093c0307d9663c8cee422e4f4b9d
Arshya GHAVAMI <arshyaghavami@Arshyas-MacBook-Air.local>
2021-02-14 13:23:21 -0800

    First Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Arshya GHAVAMI on 2/13/21.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
